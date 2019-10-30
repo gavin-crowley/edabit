bd77e8fd1bbf36353b9070cb7ea53eb60b40e977
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2019-10-30 11:47:39 +0400

    Oct 30v2

---
+++ loops.js
@@ -0,0 +1,56 @@
+// Create a function that takes a number (from 1 - 60) and returns a corresponding string of hyphens.
+function Go(num) {
+	return "-".repeat(num);
+}
+// else.g. Go(5) ➞ "-----"
+
+
+
+// Write a function that takes an integer and returns a string with the given number of "a"s in Edabit.
+function howManyTimes(num) {
+	return 'Ed' + "a".repeat(num) + 'bit';
+}
+
+
+// Additive Inverse
+function additiveInverse(arr) {
+	for ( var i = 0; i < arr.length; i++) {
+		arr[i] = -arr[i];
+	}
+	return arr;
+}
+
+function additiveInverse(arr) {
+	return arr.map(x => -x);
+}
+// additiveInverse([5, -7, 8, 3]) ➞ [-5, 7, -8, -3]
+
+
+
+// Create a function that adds a string ending to each member in an array.
+function addEnding(arr, ending) {
+	return arr.map(word => word + ending);
+}
+
+function addEnding(arr, ending) {
+	for(var i=0; i < arr.length; i++){
+		arr[i] = arr[i]+ending;
+	}
+	return arr;
+}
+// addEnding(["clever", "meek", "hurried", "nice"], "ly")
+// ➞ ["cleverly", "meekly", "hurriedly", "nicely"]
+
+
+
+// Generate a Countdown of Numbers in an Array
+function countdown(start) {
+	let arr = []
+	for(let i=start; i>=0; i--){
+		arr.push(i)
+	}
+	return arr
+}
+
+const countdown = start => [...Array(start + 1).keys()].reverse();
+// countdown(5) ➞ [5, 4, 3, 2, 1, 0]
--- objects.js
+++ objects.js
@@ -1,25 +1,21 @@
 // Extract City Facts
 function cityFacts(city) {
-	return `${city.name} has a population of ${city.population} and is situated in ${city.continent}`;
+  return `${city.name} has a population of ${city.population} and is situated in ${city.continent}`;
 }
-
-
 
 // Does the Object Contain a Given Key?
 function hasKey(obj, key) {
-	return key in obj;
+  return key in obj;
 }
-
 
 // Check if Number is within a Given Range
 function isInRange(num, range) {
-	return range.min <= num && range.max >= num;
+  return range.min <= num && range.max >= num;
 }
-
 
 // Write a function that returns true if an object is empty, and false otherwise.
 function isEmpty(obj) {
-	return !Object.keys(obj).length
+  return !Object.keys(obj).length;
 }
 
 // Create a function that takes an object and returns the keys and values as separate arrays.
@@ -30,21 +26,51 @@ function keysAndValues(obj) {
 }
 
 function keysAndValues(obj) {
-  let arr1 = [], arr2 = []
+  let arr1 = [],
+    arr2 = [];
   for (let key in obj) {
-    arr1.push(key)
-    arr2.push(obj[key])
+    arr1.push(key);
+    arr2.push(obj[key]);
   }
-  return [arr1, arr2]
+  return [arr1, arr2];
 }
 
 // Write a function that converts an object into an array, where each element represents a key-value pair.
 // toArray({ a: 1, b: 2 }) ➞ [["a", 1], ["b", 2]]
 function toArray(obj) {
-	return Object.entries(obj);
+  return Object.entries(obj);
 }
 
 // Check If objOne Is Equal to objTwo
 function isEqual(objOne, objTwo) {
-	return JSON.stringify(objOne) === JSON.stringify(objTwo);
+  return JSON.stringify(objOne) === JSON.stringify(objTwo);
 }
+
+// Write a function that takes a string and calculates the number of letters and digits within it. Return the result as an object.
+const countAll = str => ({
+  LETTERS: (str.match(/[a-z]/gi) || []).length,
+  DIGITS: (str.match(/\d/g) || []).length
+});
+//   e.g. countAll("Hello World") ➞ {"LETTERS":  10, "DIGITS": 0}
+
+
+
+// Create a function that returns the frequency distribution of an array. This function should return an object, where the keys are the unique elements and the values are the frequency in which those elements occur.
+const getFrequencies = arr =>
+  arr.reduce((map, item) => {
+    map[item] = map[item] + 1 || 1;
+    return map;
+  }, {});
+
+// e.g. getFrequencies(["A", "B", "A", "A", "A"]) ➞ { A: 4, B: 1 }
+
+
+// One of the fun parts of JavaScript is that you can extend all the standard types by extending their prototype. In this challenge, you need to give JavaScript Strings a swapCase() function, which will return a new string with all upper case characters swapped for lower case characters, and vice versa. Any non-alphabetic characters should be kept as they are.
+
+String.prototype.swapCase = function() {
+    return Array.from(this, char =>
+      char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase()
+    ).join('');
+  };
+
+// e.g. "Hello".swapCase() ➞ "hELLO"
