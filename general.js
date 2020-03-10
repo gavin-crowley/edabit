5a83f8258261a98feffc2a79ad650893581bd547
gavin-crowley <42537387+gavin-crowley@users.noreply.github.com>
2020-03-10 17:04:34 +0400

    init

--- general.js
+++ general.js
@@ -189,7 +189,7 @@ k ** k
 
 
 /////////////////////////asdasda/////asdasd//////asdasd/===========///asdad///////////////asdaasdada//////asdasads//asasdasdaddaasdasdaddasd////////////?asdasdsa?????asdadsad/////////////////////asdasd
-// sdfsfdssdfsdfasdadsasasdadsaasdasd
+// sdfsfdssdfsdfasdadsasasdadsaasdasdasdadasd
 if (num1 + num2 === 24) {
   return 'added';
 } else if (num1 - num2 === 24) {
