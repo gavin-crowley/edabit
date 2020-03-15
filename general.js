6a28f39217758102032dc37092eec667849dcbbd
gavin-crowley <42537387+gavin-crowley@users.noreply.github.com>
2020-03-15 20:46:36 +0400

    init

--- general.js
+++ general.js
@@ -188,7 +188,7 @@ k ** k
 
 
 
-/////////////////////////asdadasdasda/////asdasd/adasdas/////asdasd/===========///asdad///////////////asdaasdada//////asdasads//asasdasdaddaasdasdaddasd////////////?asdasdsa?????asdadsad/////////////////////asdasd
+///////////////////asdsadsa//////asdadasdasda/////asdasd/adasdas/////asdasd/===========///asdad///////////////asdaasdada//////asdasads//asasdasdaddaasdasdaddasd////////////?asdasdsa?????asdadsad/////////////////////asdasd
 // sdfsfdssdfsdfasdadsasasdadsaasdasdasdadasd
 if (num1 + num2 === 24) {
   return 'added';
