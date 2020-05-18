869170350c9f97a23294b3d4e1ef5021707180a6
gavin-crowley <42537387+gavin-crowley@users.noreply.github.com>
2020-05-18 09:28:47 +0400

    May 18

--- daily-practice.js
+++ daily-practice.js
@@ -3,3 +3,15 @@
 function findPerimeter(height, width) {
     return (height + width) * 2;
 }
+
+
+// Create a function that takes an array of two numbers and checks if the square root of the first number is equal to the cube root of the second number.
+
+function checkSquareAndCube(arr) {
+    return Math.sqrt(arr[0]) === Math.cbrt(arr[1])
+}
+
+// or 
+function checkSquareAndCube([a, b] = arr) {
+    return Math.sqrt(a) === Math.cbrt(b)
+}
