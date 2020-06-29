abb43b0cbfcdf53db779f13c1d594aebc6116cb9
gavin-crowley <42537387+gavin-crowley@users.noreply.github.com>
2020-06-29 13:04:24 +0400

    June 29v2

--- general.js
+++ general.js
@@ -188,7 +188,7 @@ k ** k
 
 
 
-///////////asdasdads////asdasaaqwsasdasdasdaseqweqweqwesdasdasdasdsdadasdasdasdasdasddaasdasdasasdadasaasdaasdasdasdasdasdadasdadasdasdasdasdasasdasdasd
+///////////asdasdads////asdasasdqweqweqweaaqwsasdasdasdaseqweqweqwesdasdasdasdsdadasdasdasdasdasddaasdasdasasdadasaasdaasdasdasdasdasdadasdadasdasdasdasdasasdasdasd
 if (num1 + num2 === 24) {
   return 'added';
 } else if (num1 - num2 === 24) {
